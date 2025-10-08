b77a25207d0ef50439d4e1fa548786068200ad52
CWWhitney <whitney.cory@gmail.com>
2025-10-08 06:40:53 +0200

    add confidence scores

---
+++ R/add_confidence_scores.R
@@ -0,0 +1,26 @@
+# Add confidence scores for method detection
+add_confidence_scores <- function(data) {
+  data %>%
+    mutate(
+      # Confidence based on where method was detected
+      method_confidence = case_when(
+        methods_from_title != "" & detected_methods != "" ~ "high",
+        methods_from_title != "" ~ "medium",
+        detected_methods != "" ~ "medium",
+        TRUE ~ "low"
+      ),
+      
+      # Number of methods detected
+      method_count = str_count(detected_methods, ";") + 1,
+      method_count = ifelse(detected_methods == "", 0, method_count),
+      
+      # Has VOI specifically (your key interest)
+      has_voi = str_detect(tolower(full_text), "\\bvalue of information\\b|\\bvoi\\b"),
+      
+      # Has uncertainty methods
+      has_uncertainty = str_detect(tolower(full_text), 
+                                   "\\buncertainty|\\bprobabilistic|\\bsensitivity|\\brisk\\b")
+    )
+}
+
+clean_bib_data <- add_confidence_scores(clean_bib_data)
