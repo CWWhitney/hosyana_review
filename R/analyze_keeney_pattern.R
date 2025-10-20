c996a60140214deb39bad183b76c904d13a57209
CWWhitney <whitney.cory@gmail.com>
2025-10-20 21:19:52 +0100

    remove percent calcualtion step in keeney analysis

--- R/analyze_keeney_pattern.R
+++ R/analyze_keeney_pattern.R
@@ -3,7 +3,6 @@ analyze_keeney_pattern <- function(bib_data) {
     filter(primary_intensity != "Unclassified") %>%
     count(primary_intensity, sort = TRUE) %>%
     mutate(
-      percentage = n / sum(n) * 100,
       keeney_prediction = case_when(
         primary_intensity %in% c("Level 1: Intuitive Decisions", "Level 2: Simple Analysis") ~ "High frequency (Keeney prediction)",
         TRUE ~ "Lower frequency (Keeney prediction)"
