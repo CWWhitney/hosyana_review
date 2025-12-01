990a588614b22d8f7769f20509db1c035d280c9a
CWWhitney <whitney.cory@gmail.com>
2025-12-01 16:03:41 +0100

    move functions into main index file (or remove) simplify process

--- R/prisma_config.R
+++
@@ -1,8 +0,0 @@
-# PRISMA exclusion criteria
-prisma_criteria <- list(
-  duplicates = c("duplicate", "copy", "reprint"),
-  document_types = c("annual report", "syllabus", "catalog", "course notes", 
-                     "legal document", "bibliography collection"),
-  languages = c("non-english"),
-  content_irrelevant = c("unrelated", "not relevant", "no decision focus")
-)
